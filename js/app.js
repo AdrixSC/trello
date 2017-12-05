4cbc1aaae1a1d64e2926f99d6090e2bd10b33746
AdrixSC <adriana.be@outlook.com>
2017-12-05 14:15:40 -0600

    agragando el placeholder

--- js/app.js
+++ js/app.js
@@ -1,19 +1,20 @@
 var account = 0;
-var container = document.getElementById('tasks');
+var seccionArticle = document.getElementById('tasks');
 var submitButton = document.getElementById('submit-button');
-
+var textArea = document.getElementById('textarea');
 var remove = function() {
     console.log(this);
     this.parentNode.remove();
     //this.parentElement.remove();
 }
 
-//declarando variable para agregar tarjeta en la lista que el usuario ha creado
-//revisar con el squad y jedi
-var linkAremi = function(event) {
-    event.preventDefault();
-    console.log('haciendo click');
-
+var buttonOff = function(){
+  var campoTexto = textArea.value.length;
+  if(campoTexto === 0){
+    submitButton.disabled = true;
+  }else{
+    submitButton.disabled = false;
+  }
 }
 
 //declarando variable para agregar el evento, que agregará la tarea que el usuario ingresará
@@ -32,11 +33,6 @@ var taskAdd = function(event) {
 
     var id = 'check' + account;
 
-    //agregamos atributos a los elementos de article
-    /*input.type = 'checkbox'; // esta forma es igual a hacer lo sig: checkbox.setAttribute ("type","checkbox")
-    input.addEventListener('click', throughTask); //funcion declarada más abajo...
-    input.id = id;*/
-
     label.setAttribute('for', id);
     label.textContent = task.value;
 
@@ -45,7 +41,7 @@ var taskAdd = function(event) {
     button.addEventListener('click', remove);*/
     var addCard = button;
     addCard.innerText = 'Agregar una tarjeta';
-    addCard.addEventListener('click', linkAremi);
+    addCard.addEventListener('click', link);
 
     //agregamos atributos al padre que es el article
     //article.appendChild(input);
@@ -53,25 +49,29 @@ var taskAdd = function(event) {
     article.appendChild(a);
     //a.appendChild(contentA);
     article.appendChild(button);
-    container.appendChild(article);
+    seccionArticle.appendChild(article);
     account++;
 
-    task.value = " "; //esto sirve para limpiar o borrar el contenido que el usuario agregó
 
+    task.value = ""; //esto sirve para limpiar o borrar el contenido que el usuario agregó
+    task.placeholder = "Añadir lista";
 
 };
 
-//declarando funcion para hacer el check list de las tareas que el usuario ha realizado ya
-var throughTask = function() {
-    var checked = this.checked; //con this hacemos referencia a qué se le está haciendo el check
-    //console.log(checked);
-    var text = this.nextSibling; //metodo para tachar el texto del checked, en donde el texto a tachar es el hermano del cuadro al que se le hace el check
-    //console.log(text);
-    if (checked) { //aplicando la condición en donde si el usuario le da check al cuadrito, se agregue la linea encima del texto, con el estilo text decoration
-        text.style.textDecoration = 'line-through';
-    } else { //si no se cumple la condición no aplica el estilo de rayar el texto, con none
-        text.style.textDecoration = 'none';
-    }
+//declarando variable para agregar tarjeta en la lista que el usuario ha creado
+//revisar con el squad y jedi
+var link = function(event) {
+    event.preventDefault();
+    console.log('haciendo click');
+    var task = document.getElementById('text');
+    var article = document.createElement('article');
+    var label = document.createElement('label');
+    var id = 'check' + account;
+    label.setAttribute('for', id);
+    label.textContent = task.value;
+
 };
+
 
 submitButton.addEventListener('click', taskAdd);
+submitButton.addEventListener('keyup', buttonOff);
